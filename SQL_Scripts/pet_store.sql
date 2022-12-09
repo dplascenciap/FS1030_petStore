f6e6103da439adc38f9c29c22c790c2d81935259
dplascenciap <dplascenciap@gmail.com>
2022-12-09 16:53:16 -0500

    Database created in local environment

---
+++ SQL_Scripts/pet_store.sql
@@ -0,0 +1,16 @@
+-- https://github.com/maurom88/YorkU_CSFS1030.git
+-- Show all databases.
+SHOW DATABASES;
+
+-- Create a database and name it pet_store_db.
+CREATE DATABASE pet_store_db;
+
+-- Show all databases again, 'pet_store_db' should be displayed.
+SHOW DATABASES;
+
+-- Select the database 'pet_store_db' to be used.`
+USE pet_store_db;
+
+-- Show the tables in the database.
+-- At this point there are no tables in the database
+SHOW TABLES;

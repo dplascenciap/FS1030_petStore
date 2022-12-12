cffdd952376e4e7ffa56c6487ba42a1fcb619729
dplascenciap <dplascenciap@gmail.com>
2022-12-12 14:51:26 -0500

    Include scripts to populate the database

---
+++ SQL_Scripts/MOCK_Data/customer.sql
@@ -0,0 +1,58 @@
+create table customer (
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(50),
+	phone_number VARCHAR(50),
+	address VARCHAR(50)
+);
+insert into customer (first_name, last_name, email, phone_number, address) values ('Kara', 'Maypes', 'kmaypes0@google.es', '520-995-2958', '17451 Dorton Parkway');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Philis', 'Benettini', 'pbenettini1@studiopress.com', '257-229-8176', '63 Golf View Point');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Charles', 'Swinley', 'cswinley2@sina.com.cn', '949-541-9473', '2 Ridge Oak Parkway');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Collete', 'Adie', 'cadie3@blogger.com', '277-943-2470', '4 Rigney Drive');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Briant', 'Northin', 'bnorthin4@youku.com', '543-959-9662', '5064 Columbus Place');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Phillipp', 'Featherstonhaugh', 'pfeatherstonhaugh5@reddit.com', '206-850-7754', '299 Calypso Drive');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Riane', 'Aldrick', 'raldrick6@digg.com', '821-420-2423', '8467 Bultman Park');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Reba', 'Palke', 'rpalke7@apple.com', '537-769-3660', '06781 Mcguire Crossing');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Lannie', 'Joselin', 'ljoselin8@spotify.com', '432-339-6768', '50 Anniversary Place');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Lonni', 'Britney', 'lbritney9@loc.gov', '124-382-6281', '8 Pine View Circle');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Kliment', 'Musprat', 'kmusprata@cpanel.net', '487-943-7075', '65 Sunnyside Trail');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Cecile', 'Arnaudi', 'carnaudib@tripod.com', '744-162-5610', '0595 Maywood Plaza');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Timmie', 'Ahrend', 'tahrendc@cpanel.net', '742-676-1265', '481 Washington Drive');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Genny', 'Jenks', 'gjenksd@com.com', '639-423-6120', '9 Arizona Avenue');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Ingram', 'Paskerful', 'ipaskerfule@altervista.org', '611-242-5887', '82381 Victoria Point');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Ad', 'Leal', 'alealf@w3.org', '413-986-1000', '735 Kedzie Court');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Upton', 'Dillaway', 'udillawayg@livejournal.com', '587-420-2500', '0 Corben Junction');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Marybeth', 'Gierhard', 'mgierhardh@paypal.com', '601-268-2282', '39973 Westport Court');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Roch', 'Trangmar', 'rtrangmari@npr.org', '878-191-0620', '1791 Manufacturers Parkway');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Linoel', 'Mawne', 'lmawnej@sciencedaily.com', '902-750-8565', '56384 Cody Crossing');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Danie', 'Cund', 'dcundk@creativecommons.org', '118-926-3610', '5155 Chive Road');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Thibaut', 'Rehor', 'trehorl@goodreads.com', '180-662-9615', '6556 Jay Pass');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Camille', 'Abrahamowitcz', 'cabrahamowitczm@arizona.edu', '703-348-3704', '4 Dakota Point');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Fabian', 'Trevers', 'ftreversn@cnet.com', '338-173-3783', '232 Nancy Place');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Sammie', 'Toop', 'stoopo@51.la', '650-217-2973', '21857 Mesta Alley');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Margeaux', 'Gowans', 'mgowansp@addtoany.com', '173-990-6009', '19728 Arkansas Road');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Hertha', 'Ferryman', 'hferrymanq@wikispaces.com', '757-108-3697', '436 Duke Avenue');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Courtney', 'Pearsall', 'cpearsallr@harvard.edu', '853-371-1868', '48120 Bayside Trail');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Auberon', 'Syme', 'asymes@moonfruit.com', '682-210-3335', '754 Evergreen Hill');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Ana', 'Clemendet', 'aclemendett@pagesperso-orange.fr', '789-163-8620', '01690 Wayridge Hill');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Lainey', 'Goaks', 'lgoaksu@reddit.com', '644-234-0129', '7914 4th Alley');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Guthrie', 'Jendrusch', 'gjendruschv@mayoclinic.com', '213-309-7657', '5427 Spenser Plaza');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Ingar', 'Wyleman', 'iwylemanw@opensource.org', '388-800-6131', '4217 Pearson Hill');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Thorpe', 'Glanton', 'tglantonx@prweb.com', '217-294-1828', '1260 Fremont Lane');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Grier', 'Travers', 'gtraversy@marriott.com', '906-486-4267', '470 Sundown Drive');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Ahmad', 'Coleson', 'acolesonz@usatoday.com', '768-994-9376', '55 Westerfield Plaza');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Towney', 'Crooke', 'tcrooke10@creativecommons.org', '322-695-8451', '76 Debs Place');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Saul', 'Dudhill', 'sdudhill11@redcross.org', '802-622-5837', '6575 Eggendart Plaza');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Fabe', 'Shelton', 'fshelton12@skyrock.com', '567-428-1237', '082 Bartillon Park');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Suzanne', 'Acutt', 'sacutt13@mozilla.org', '997-307-0542', '8 Loeprich Parkway');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Marwin', 'Bagguley', 'mbagguley14@jigsy.com', '103-598-3715', '7 Stoughton Road');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Fleurette', 'Butchard', 'fbutchard15@answers.com', '509-425-7198', '97357 La Follette Hill');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Lottie', 'Balffye', 'lbalffye16@infoseek.co.jp', '998-276-5414', '598 Buhler Court');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Rubia', 'O''Meara', 'romeara17@tripod.com', '376-576-2060', '35934 Calypso Avenue');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Giusto', 'Libreros', 'glibreros18@sohu.com', '351-146-5702', '811 1st Way');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Antoinette', 'Divver', 'adivver19@nature.com', '478-819-3038', '23 Birchwood Park');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Gun', 'Goublier', 'ggoublier1a@over-blog.com', '462-747-0938', '1140 3rd Place');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Chip', 'Dohrmann', 'cdohrmann1b@google.it', '229-675-2097', '88653 Raven Pass');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Neala', 'Harp', 'nharp1c@cyberchimps.com', '896-275-7633', '1 Lawn Trail');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Tades', 'Medway', 'tmedway1d@nature.com', '662-445-0415', '2 Green Plaza');
+insert into customer (first_name, last_name, email, phone_number, address) values ('Andy', 'Goldring', 'agoldring1e@ibm.com', '196-269-8302', '0 Moulton Crossing');
